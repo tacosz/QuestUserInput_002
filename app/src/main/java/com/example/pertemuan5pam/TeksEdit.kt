3443e9af12f7dd6675f112a20da995c3098ef738
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-10-31 15:26:21 +0700

    membuat button serta mengatur agar button dapat di clik dengan ketentuan

--- app/src/main/java/com/example/pertemuan5pam/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuan5pam/TeksEdit.kt
@@ -3,9 +3,11 @@ package com.example.pertemuan5pam
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -20,6 +22,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -79,5 +82,16 @@ fun FormDataDiri(modifier: Modifier)
             thickness = dimensionResource(id = R.dimen.divider_tipis),
             color = Color.DarkGray
         )
+        Button(
+            modifier = Modifier.fillMaxWidth(1f),
+            enabled = textAlamat.isNotEmpty(),
+            onClick = {
+                nama = textNama
+                jenis = textJK
+                alamat = textAlamat
+            }
+        ) {
+            Text(stringResource(id = R.string.submit))
+        }
     }
 }
