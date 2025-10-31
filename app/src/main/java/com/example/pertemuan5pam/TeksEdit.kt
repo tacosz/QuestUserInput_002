3dcfdb830abf55da5778ebab9022004efe5b2b88
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-10-31 15:04:38 +0700

    membuat fungsi foreach dan row didalam row untuk jenis kelamin

--- app/src/main/java/com/example/pertemuan5pam/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuan5pam/TeksEdit.kt
@@ -1,9 +1,12 @@
 package com.example.pertemuan5pam
 
+import androidx.compose.animation.core.withInfiniteAnimationFrameMillis
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -42,5 +45,15 @@ fun FormDataDiri(modifier: Modifier)
                 textNama = it
             }
         )
+        Row {
+            gender.forEach { item ->
+                Row(modifier = Modifier.selectable(
+                    selected = textJK == item,
+                    onClick = {textJK = item}
+                ), verticalAlignment = Alignment.CenterVertically) {
+
+                }
+            } }
+        }
     }
 }
