3a34b87d2d799a271efe28b0fd958c918b29a8f0
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-10-31 15:29:11 +0700

    membuat, mengatur horizontal divider

--- app/src/main/java/com/example/pertemuan5pam/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuan5pam/TeksEdit.kt
@@ -93,5 +93,13 @@ fun FormDataDiri(modifier: Modifier)
         ) {
             Text(stringResource(id = R.string.submit))
         }
+
+        HorizontalDivider(
+            modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_medium),
+                top = dimensionResource(id = R.dimen.padding_medium
+                )),
+            thickness = dimensionResource(id = R.dimen.divider_tipis),
+            color = Color.DarkGray
+        )
     }
 }
