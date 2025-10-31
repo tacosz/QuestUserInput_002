d441ccc427875ae7c954e44eb5e25f703df87b71
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-10-31 14:55:40 +0700

    merubah pemanggilan function lain

--- app/src/main/java/com/example/pertemuan5pam/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan5pam/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Pertemuan5PAMTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    FormDataDiri(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
