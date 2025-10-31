59d36a7781d421044b4e6aba2c294cd37414bab4
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-10-31 14:49:27 +0700

    membuat variabel untuk menyimpan data yg diperoleh dari komponen UI

--- app/src/main/java/com/example/pertemuan5pam/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuan5pam/TeksEdit.kt
@@ -1,14 +1,20 @@
 package com.example.pertemuan5pam
 
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
+@Composable
 fun FormDataDiri(modifier: Modifier)
 {
     var textNama by remember { mutableStateOf(value = "") }
     var textAlamat by remember { mutableStateOf(value = "") }
     var textJK by remember { mutableStateOf(value = "") }
+
+    var nama by remember { mutableStateOf(value = "") }
+    var alamat by remember { mutableStateOf(value = "") }
+    var jenis by remember { mutableStateOf(value = "") }
 }
