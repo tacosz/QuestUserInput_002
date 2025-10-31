d3df3c0d9a160a54c5e3b0c7932fba9a702374b7
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-10-31 15:20:04 +0700

    membuat nmodifier pada horizontal divider

--- app/src/main/java/com/example/pertemuan5pam/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuan5pam/TeksEdit.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
@@ -17,6 +18,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -68,6 +70,11 @@ fun FormDataDiri(modifier: Modifier)
             onValueChange = {
                 textAlamat = it
             }
+        )
+
+        HorizontalDivider(
+            modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_medium), top = dimensionResource(id = R.dimen.padding_medium)),
+
         )
     }
 }
