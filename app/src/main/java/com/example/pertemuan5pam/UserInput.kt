a3c466079f7f616b8229683102072096a420b8d1
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-01 20:45:49 +0700

    membuat desain UI text dan card

--- app/src/main/java/com/example/pertemuan5pam/UserInput.kt
+++ app/src/main/java/com/example/pertemuan5pam/UserInput.kt
@@ -2,7 +2,13 @@ package com.example.pertemuan5pam
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -11,6 +17,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun FormRegistration (modifier: Modifier)
@@ -43,5 +53,19 @@ fun FormRegistration (modifier: Modifier)
         )
     }
 
-
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 80.dp, start = 40.dp, end = 40.dp, bottom = 30.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.formregistration),
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Card(
+            modifier = Modifier.fillMaxSize()
+        ) {  }
+    }
 }
