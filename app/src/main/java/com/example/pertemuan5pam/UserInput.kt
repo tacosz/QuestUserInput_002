cd8fe1651f325c1b176a1efb939833174f7e2b8d
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-01 19:46:39 +0700

    membuat variabel untuk mengingat nilai yang diisi dari keyboard

--- app/src/main/java/com/example/pertemuan5pam/UserInput.kt
+++ app/src/main/java/com/example/pertemuan5pam/UserInput.kt
@@ -1,8 +1,18 @@
 package com.example.pertemuan5pam
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 
+@Composable
 fun FormRegistration (modifier: Modifier)
 {
-
+    var textNama by remember { mutableStateOf(value = "") }
+    var textAsal by remember { mutableStateOf(value = "") }
+    var textTgl by remember { mutableStateOf(value = "") }
+    var textRT by remember { mutableStateOf(value = "") }
+    var textRW by remember { mutableStateOf(value = "") }
+    var textUmur by remember { mutableStateOf(value = "") }
+    var textJK by remember { mutableStateOf(value = "") }
 }
