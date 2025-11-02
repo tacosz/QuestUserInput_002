96be136d33093a1094c2ba3d90b07597a53004e3
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2025-11-02 15:51:39 +0700

    membuat private fun untuk date

--- app/src/main/java/com/example/pertemuan5pam/UserInput.kt
+++ app/src/main/java/com/example/pertemuan5pam/UserInput.kt
@@ -1,5 +1,6 @@
 package com.example.pertemuan5pam
 
+import android.icu.text.SimpleDateFormat
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -32,7 +33,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.Button
@@ -44,13 +44,19 @@ import androidx.compose.material3.VerticalDivider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.text.input.KeyboardType
 import kotlin.collections.forEach
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+private fun convertMillisToDate(millis: Long): String {
+    val formatter = java.text.SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+    return formatter.format(Date(millis))
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FormRegistration (modifier: Modifier)
-{
+fun FormRegistration (modifier: Modifier) {
     var textNama by remember { mutableStateOf(value = "") }
     var textAsal by remember { mutableStateOf(value = "") }
     var showDatePicker by remember { mutableStateOf(false) }
@@ -58,7 +64,11 @@ fun FormRegistration (modifier: Modifier)
     val selectedDate = datePickerState.selectedDateMillis?.let {
         val calendar = Calendar.getInstance()
         calendar.timeInMillis = it
-        "${calendar.get(Calendar.DAY_OF_MONTH)}/${calendar.get(Calendar.MONTH) + 1}/${calendar.get(Calendar.YEAR)}"
+        "${calendar.get(Calendar.DAY_OF_MONTH)}/${calendar.get(Calendar.MONTH) + 1}/${
+            calendar.get(
+                Calendar.YEAR
+            )
+        }"
     } ?: ""
 
     var textRT by remember { mutableStateOf(value = "") }
@@ -77,7 +87,7 @@ fun FormRegistration (modifier: Modifier)
     var submitted by remember { mutableStateOf(false) }
     var jk by remember { mutableStateOf(value = "") }
 
-    val gender: List<String> = listOf("Laki-laki","Perempuan")
+    val gender: List<String> = listOf("Laki-laki", "Perempuan")
 
     val image = painterResource(R.drawable.bg)
 
@@ -88,161 +98,170 @@ fun FormRegistration (modifier: Modifier)
             contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxSize()
         )
-    }
-
-    Column (
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(top = 80.dp, start = 40.dp, end = 40.dp, bottom = 30.dp),
-    ) {
-        Text(
-            text = stringResource(R.string.formregistration),
-            fontSize = 40.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-        Card(
+        Column(
             modifier = Modifier
                 .fillMaxSize()
+                .padding(top = 80.dp, start = 40.dp, end = 40.dp, bottom = 30.dp),
         ) {
-            OutlinedTextField(
-                value = textNama,
-                singleLine = true,
-                shape = MaterialTheme.shapes.large,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 30.dp, start = 15.dp, end = 15.dp),
-                label = {Text(text = "Nama Lengkap")},
-                onValueChange = {
-                    textNama = it
-                }
+            Text(
+                text = stringResource(R.string.formregistration),
+                fontSize = 40.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 16.dp)
             )
-
-            OutlinedTextField(
-                value = textAsal,
-                singleLine = true,
-                shape = MaterialTheme.shapes.large,
+            Card(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(start = 15.dp, end = 15.dp),
-                label = {Text(text = "Kota Asal")},
-                onValueChange = {
-                    textAsal = it
-                }
-            )
-
-            Row (
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 15.dp, start = 20.dp, end = 20.dp),
-                horizontalArrangement = Arrangement.spacedBy(10.dp)
+                    .fillMaxSize()
             ) {
                 OutlinedTextField(
-                    value = selectedDate,
+                    value = textNama,
                     singleLine = true,
                     shape = MaterialTheme.shapes.large,
-                    onValueChange = { },
-                    readOnly = true,
-                    label = {Text(text = "Tanggal Lahir")},
-                    trailingIcon = {
-                        IconButton(
-                            onClick = {showDatePicker = !showDatePicker}
-                        ) {
-                            Icon(
-                                imageVector = Icons.Default.DateRange,
-                                contentDescription = "Select Date"
-                            )
-                        }
-                    },
-                    modifier = Modifier.weight(1f)
+                    modifier = Modifier
                         .fillMaxWidth()
-                        .padding(top = 12.dp)
-                        .clickable(enabled = true, onClick = {showDatePicker = true})
-                )
-                OutlinedTextField(
-                    value = textRT,
-                    singleLine = true,
-                    shape = MaterialTheme.shapes.large,
-                    modifier = Modifier
-                        .width(60.dp)
-                        .padding(top = 12.dp),
+                        .padding(top = 30.dp, start = 15.dp, end = 15.dp),
+                    label = { Text(text = "Nama Lengkap") },
                     onValueChange = {
-                        textRT = it
-                    },
-                    label = {Text(text = "RT")}
-                )
-                VerticalDivider(
-                    thickness = dimensionResource(id = R.dimen.divider_tipis),
-                    modifier = Modifier.padding(top = 30.dp).height(40.dp)
-                )
-                OutlinedTextField(
-                    value = textRW,
-                    singleLine = true,
-                    shape = MaterialTheme.shapes.large,
-                    modifier = Modifier
-                        .width(60.dp)
-                        .padding(top = 12.dp),
-                    onValueChange = {
-                        textRW = it
-                    },
-                    label = {Text(text = "RW")
+                        textNama = it
                     }
                 )
-            }
-            OutlinedTextField(
-                value = textUmur,
-                singleLine = true,
-                shape = MaterialTheme.shapes.large,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(start = 15.dp, end = 15.dp),
-                label = {Text(text = "Umur")},
-                onValueChange = {
-                    textUmur = it
+
+                OutlinedTextField(
+                    value = textAsal,
+                    singleLine = true,
+                    shape = MaterialTheme.shapes.large,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 15.dp, end = 15.dp),
+                    label = { Text(text = "Kota Asal") },
+                    onValueChange = {
+                        textAsal = it
+                    }
+                )
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 15.dp, start = 20.dp, end = 20.dp),
+                    horizontalArrangement = Arrangement.spacedBy(10.dp)
+                ) {
+                    OutlinedTextField(
+                        value = selectedDate,
+                        singleLine = true,
+                        shape = MaterialTheme.shapes.large,
+                        onValueChange = { },
+                        readOnly = true,
+                        label = { Text(text = "Tanggal Lahir") },
+                        trailingIcon = {
+                            IconButton(
+                                onClick = { showDatePicker = !showDatePicker }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.DateRange,
+                                    contentDescription = "Select Date"
+                                )
+                            }
+                        },
+                        modifier = Modifier.weight(1f)
+                            .fillMaxWidth()
+                            .padding(top = 12.dp)
+                            .clickable(enabled = true, onClick = { showDatePicker = true })
+                    )
+                    OutlinedTextField(
+                        value = textRT,
+                        singleLine = true,
+                        shape = MaterialTheme.shapes.large,
+                        modifier = Modifier
+                            .width(60.dp)
+                            .padding(top = 12.dp),
+                        onValueChange = {
+                            textRT = it
+                        },
+                        label = { Text(text = "RT") }
+                    )
+                    VerticalDivider(
+                        thickness = dimensionResource(id = R.dimen.divider_tipis),
+                        modifier = Modifier.padding(top = 30.dp).height(40.dp)
+                    )
+                    OutlinedTextField(
+                        value = textRW,
+                        singleLine = true,
+                        shape = MaterialTheme.shapes.large,
+                        modifier = Modifier
+                            .width(60.dp)
+                            .padding(top = 12.dp),
+                        onValueChange = {
+                            textRW = it
+                        },
+                        label = {
+                            Text(text = "RW")
+                        }
+                    )
                 }
-            )
-            Text(stringResource(R.string.jk),
-                modifier = Modifier
-                    .align(Alignment.Start)
-                    .padding(start = 24.dp, top = 12.dp),
-                fontWeight = FontWeight.Bold
-            )
-            Row {
-                gender.forEach { item ->
-                    Row(modifier = Modifier.padding(start = 12.dp).selectable(
-                        selected = textJK == item,
-                        onClick = {textJK = item}
-                    ), verticalAlignment = Alignment.CenterVertically) {
-                        RadioButton(
+                OutlinedTextField(
+                    value = textUmur,
+                    singleLine = true,
+                    shape = MaterialTheme.shapes.large,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 15.dp, end = 15.dp),
+                    label = { Text(text = "Umur") },
+                    onValueChange = {
+                        textUmur = it
+                    }
+                )
+                Text(
+                    stringResource(R.string.jk),
+                    modifier = Modifier
+                        .align(Alignment.Start)
+                        .padding(start = 24.dp, top = 12.dp),
+                    fontWeight = FontWeight.Bold
+                )
+                Row {
+                    gender.forEach { item ->
+                        Row(
+                            modifier = Modifier.padding(start = 12.dp).selectable(
                             selected = textJK == item,
-                            onClick = {
-                                textJK = item
-                            })
-                        Text(text = item)
+                            onClick = { textJK = item }
+                        ), verticalAlignment = Alignment.CenterVertically) {
+                            RadioButton(
+                                selected = textJK == item,
+                                onClick = {
+                                    textJK = item
+                                })
+                            Text(text = item)
+                        }
                     }
                 }
+                Row(modifier = Modifier.padding(start = 40.dp, end = 40.dp)) {
+                    Checkbox(checked = check, onCheckedChange = { check = it })
+                    Text(
+                        stringResource(R.string.check),
+                        fontSize = 15.sp,
+                        color = Color.DarkGray
+                    )
+                }
+                Spacer(Modifier.height(height = 80.dp).fillMaxWidth(1f))
+                Button(
+                    enabled = check,
+                    onClick = {
+                        nama = textNama
+                        asal = textAsal
+                        tgl = selectedDate
+                        rt = textRT
+                        rw = textRW
+                        jk = textJK
+                        submitted = !submitted
+                    },
+                    modifier = Modifier.width(width = 150.dp).align(Alignment.CenterHorizontally)
+                ) {
+                    Text(stringResource(R.string.submit))
+                }
+                Spacer(Modifier.height(height = 50.dp))
             }
-            Row (modifier = Modifier.padding(start = 40.dp, end = 40.dp)) {
-                Checkbox(checked = check, onCheckedChange = {check = it})
-                Text(stringResource(R.string.check),
-                    fontSize = 15.sp,
-                    color = Color.DarkGray)
-            }
-            Spacer(Modifier.height(height = 80.dp).fillMaxWidth(1f))
-            Button(enabled = check,
-                onClick = {
-                    nama = textNama
-                    asal = textAsal
-                    tgl = selectedDate
-                    rt = textRT
-                    rw = textRW
-                    jk = textJK
-                    submitted = !submitted
-                },
-                modifier = Modifier.width(width = 150.dp).align(Alignment.CenterHorizontally)) {
-                Text(stringResource(R.string.submit))
-            }
-            Spacer(Modifier.height(height = 50.dp))
         }
     }
 }
+
+
+
